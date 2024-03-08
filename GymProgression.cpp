0cc99cc2c2355599a41365d1cf2ec66c50f18879
Asia Harris <asia.harris1@bulldogs.aamu.edu>
2024-03-08 12:03:18 -0600

    Update GymProgression.cpp
    Created main function and included header file

---
+++ GymProgression.cpp
@@ -0,0 +1,14 @@
+/* Asia Harris
+   March 8th, 2024
+   This program is meant to track my gym progression and log changes.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
